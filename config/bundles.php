4299257ede9213fa454ee1c6f4804f9caca20aed
Thomas Gharbi <thomas.gharbi@gmail.com>
2025-08-18 22:44:07 +0200

    [SETUP|UX] Install Symfony UX Icon

--- config/bundles.php
+++ config/bundles.php
@@ -17,4 +17,5 @@ return [
     Symfony\UX\React\ReactBundle::class => ['all' => true],
     Symfony\UX\TwigComponent\TwigComponentBundle::class => ['all' => true],
     Symfony\UX\Typed\TypedBundle::class => ['all' => true],
+    Symfony\UX\Icons\UXIconsBundle::class => ['all' => true],
 ];
